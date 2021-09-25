581a1a7733e5fd079e81cae1af886fe8b3ff6fdd
Manmeet <manmeetkalsi17@gmail.com>
2021-09-25 14:55:02 +0530

    netlify

--- pages/api/casafari.js
+++ pages/api/casafari.js
@@ -1,27 +1,20 @@
-const chrome = require("chrome-aws-lambda");
-let puppeteer;
-// const puppeteer = require("puppeteer");
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  // running on the Vercel platform.
-
-  puppeteer = require("puppeteer-core");
-} else {
-  // running locally.
-  puppeteer = require("puppeteer");
-}
+const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const browser = await puppeteer.launch({
-        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-        defaultViewport: chrome.defaultViewport,
-        executablePath: await chrome.executablePath,
-        headless: true,
-        ignoreHTTPSErrors: true,
+        // Required
+        executablePath: await chromium.executablePath,
+
+        // Optional
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        headless: chromium.headless,
       }); //browser initiate
       const page = await browser.newPage(); // opening a new blank page'
-      await page.setDefaultNavigationTimeout(0);
+      // await page.setDefaultNavigationTimeout(0);
       await page.goto(
         "https://www.casafari.com/home-sale/property-" + req.body.formData.id,
         {
