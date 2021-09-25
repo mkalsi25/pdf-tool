6ff5ab16896dd815cf663637d1feaf312fb8612a
Manmeet <manmeetkalsi17@gmail.com>
2021-09-25 13:40:59 +0530

    timeout disable

--- pages/api/casafari.js
+++ pages/api/casafari.js
@@ -81,7 +81,7 @@ export default async function handler(req, res) {
           return results;
         });
         await browser.close();
-        return res.status(200).json(content), resolve(content);
+        return res.status(200).json(content);
       } catch (e) {
         return reject(e);
       }
