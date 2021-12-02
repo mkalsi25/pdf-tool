6d542bba854196ad1e75217b054e745fc27211fc
Manmeet <manmeetkalsi17@gmail.com>
2021-12-02 13:22:47 +0530

    set up headers for avoid blocks

--- pages/api/idealista.js
+++ pages/api/idealista.js
@@ -1,28 +1,46 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const headers = [
+  {
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Sec-Ch-Ua":
+      '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent":
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+  },
+  {
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent":
+      "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
+  },
+];
 export default async function handler(req, res) {
   if (req.method === "GET") {
     return res.status(202).json("404 Not found");
   } else if (req.method === "POST") {
     try {
       const url = `https://www.idealista.com/en/inmueble/${req.body.formData.listing}/`;
-      const { data } = await axios.get(url, {
-        headers: {
-          Accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-          "Accept-Encoding": "gzip, deflate, br",
-          "Accept-Language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,pa;q=0.6",
-          "User-Agent":
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
-          Referer: url,
-          "Cache-Control": "max-age=0",
-          path: `/en/inmuenle/${req.body.formData.listing}`,
-          scheme: "https",
-          method: "GET",
-          authority: "www.idealista.com",
-        },
-      });
+      const { data } = await axios.get(url, { headers: sample(headers) });
 
       const $ = cheerio.load(data);
       var ret = { url };
