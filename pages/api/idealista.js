9fcb7ba06ec5ad11071a3b8a11a6cef143b93663
Manmeet <manmeetkalsi17@gmail.com>
2022-02-14 11:29:51 +0530

    update headers

--- pages/api/idealista.js
+++ pages/api/idealista.js
@@ -1,47 +1,27 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const sample = (array) => array[Math.floor(Math.random() * array.length)];
-
-const headers = [
-  {
-    Accept:
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Sec-Ch-Ua":
-      '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent":
-      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
-  },
-  {
-    Accept:
-      "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent":
-      "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0",
-  },
-];
+// const sample = (array) => array[Math.floor(Math.random() * array.length)];
+// import { getProxies } from "../../lib/getProxies";
 export default async function handler(req, res) {
+  // const proxies = await getProxies();
   if (req.method === "GET") {
     return res.status(202).json("404 Not found");
   } else if (req.method === "POST") {
     try {
-      // console.log(req.body);
       const url = `https://www.idealista.com/en/inmueble/${req.body.id}/`;
-      const { data } = await axios.get(url, { headers: sample(headers) });
+      // const proxy = sample(proxies);
+      // console.log(proxy);
+      const { data } = await axios.get(url, {
+        headers: {
+          Accept: "*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0",
+          Referer: url,
+        },
+        // proxy: proxy,
+      });
 
       const $ = cheerio.load(data);
       var ret = { url };
